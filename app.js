7b4cd19abac8bc118cf5bdb258b9560d344173b8
Your NameMaxim <Zeppelin8686@gmail.com>
2021-07-31 02:31:22 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+$(function(){
+    
+    let header = $("#header");
+    let headerTop = $("#header-top")
+    let headerH = header.innerHeight();
+    let scrollPos = $(window).scrollTop();
+
+    $(window).on("scroll load", function () {
+        headerH = header.innerHeight();
+        scrollPos = $(this).scrollTop();
+        
+        if (scrollPos>headerH) {
+            headerTop.addClass("header__fixed");
+        }
+        else {
+            headerTop.removeClass("header__fixed");
+        }
+    });
+
+});
+
+
+let age = prompt('сколько тебе лет?');
+alert(`тебе ${age}`);
